a80f83385005b2258109c0eabe0e1ca9c25d69be
user <mdmoslehuddin2025@gmail.com>
2024-09-30 10:29:09 +0600

    social link update

--- js/login.js
+++ js/login.js
@@ -1,1 +1,7 @@
-console.log('button clicking file added')
+document.getElementById('btn-Donate-Now')
+.abbEventListener('click', function(event){
+    event.preventDefault();
+
+    const DonateNow = document.getElementById('input-Donate-Now').value;
+    console.log(DonateNow);
+})
